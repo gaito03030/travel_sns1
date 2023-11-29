ea2a04878b05604a0cc5e166569af1cb56cbd30d
miyashita-703 <miyashi2k@gmail.com>
2023-11-29 15:41:09 +0900

    はろえぶりわん追加したよ

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
 
         <title>MOKKAI
         </title>
+        <p>はろえぶりわん</p>
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
