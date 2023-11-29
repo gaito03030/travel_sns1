aeaf4dcf09b9e412caa40d3458c5226f53acab2c
PLKmei <it222060@trident.ac.jp>
2023-11-29 15:42:11 +0900

    www

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
         <title>Laravel</title>
 
         <p>こんにちは</p>
+        <p>www</p>
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
