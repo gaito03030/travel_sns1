082c0a2af73913a4baa068b82063aec2f18eb60c
gaito03030 <it222168@trident.ac.jp>
2023-11-29 15:38:43 +0900

    ただいまと変えました。

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,8 @@
         <title>MOKKAI
         </title>
 
+        <p>ただいま</p>
+
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
