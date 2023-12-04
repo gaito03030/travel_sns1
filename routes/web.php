9fa57130ca256ff072b0c89edcde3a32564fd89d
miyashita-703 <miyashi2k@gmail.com>
2023-12-04 12:16:52 +0900

    laravel/ui 再ダウンロードしました

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,7 @@ Route::get('company_mypage',[UsersController::class,'index']);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
