5d7dad949d6289140bcb9740fba06ad072a14fa0
gaito03030 <it222168@trident.ac.jp>
2023-12-19 10:51:14 +0900

    aaa

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-                'resources/js/post.js'
+                "resources/sass/app.scss",
+                "resources/js/app.js",
+                "resources/css/post_timeline.css",
+                "resources/css/userside_comp_prof",
+                "resources/js/post.js",
             ],
             refresh: true,
         }),
