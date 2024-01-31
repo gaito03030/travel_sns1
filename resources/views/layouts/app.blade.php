4baecbf9c7619bf1385e4998882d72213c74bee6
miyashita-703 <miyashi2k@gmail.com>
2024-01-31 15:33:34 +0900

    commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,5 +66,6 @@
     </main>
     </div>
     <!-- Scripts -->
-    @vite( ['resources/js/jquery-3.7.0.min.js','resources/js/app.js','resources/js/preview.js','resources/js/post.js'])
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    @vite( 'resources/js/app.js','resources/js/preview.js','resources/js/post.js'])
 </body>
--- resources/views/layouts/general.blade.php
+++ resources/views/layouts/general.blade.php
@@ -15,7 +15,8 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <!-- Scripts -->
-    @vite( ['resources/sass/app.scss','resources/js/jquery-3.7.0.min.js','resources/js/app.js','resources/js/preview.js','resources/js/post.js','resources/js/setting.js','resources/js/popup.js','resources/js/script.js'])
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    @vite( ['resources/sass/app.scss','resources/js/app.js','resources/js/preview.js','resources/js/post.js','resources/js/setting.js','resources/js/popup.js','resources/js/script.js'])
     <link rel="manifest" href="/manifest.json">
     <script>
         if ('serviceWorker' in navigator) {
