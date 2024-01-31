c8d513f2ef7cba8729fd8bac9523c1630e099879
miyashita-703 <miyashi2k@gmail.com>
2024-01-31 15:25:29 +0900

    commit

--- resources/views/layouts/company.blade.php
+++ resources/views/layouts/company.blade.php
@@ -9,6 +9,7 @@
     <title>Document</title>
     <link href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@40,200,0,0" />
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 
     <!-- css -->
     <!-- @vite('resources/css/style.css') -->
@@ -66,7 +67,7 @@
         </main>
     </div>
     <!-- Scripts -->
-    @vite( ['resources/js/jquery-3.7.0.min.js','resources/js/app.js','resources/js/preview.js','resources/js/post.js','resources/js/setting.js','resources/js/popup.js','resources/js/script.js'])
+    @vite( ['resources/js/app.js','resources/js/preview.js','resources/js/post.js','resources/js/setting.js','resources/js/popup.js','resources/js/script.js'])
 
 </body>
 
